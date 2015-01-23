ddec50ed174d48dd65e5dd7c98c1e37322be9be4
Britt Gresham <britt@puppetlabs.com>
2015-01-22 19:20:22 -0800

    Testing nginx support

--- site/modules/roles/manifests/webserver.pp
+++ site/modules/roles/manifests/webserver.pp
@@ -1,10 +1,20 @@
 class roles::webserver {
     $latest_packages = [
         'fail2ban',
-        'apache2',
     ]
 
     package { $latest_packages:
         ensure => latest,
+    }
+
+    class { 'nginx': }
+
+    # Just for testing
+    nginx::resource::vhost { ['brittg.vm', 'www.brittg.vm']:
+      www_root => '/var/www/',
+    }
+
+    nginx::resource::vhost { 'python.brittg.vm':
+      proxy => 'http://localhost:8000',
     }
 }
