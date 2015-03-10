92abcb51d4d340b3130b0fbdf86d5a229c1109c5
Britt Gresham <britt@puppetlabs.com>
2015-03-09 20:24:05 -0700

    Add webserver profile to linode

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,4 @@
 class profiles {
-    include roles::base
+    include profiles::base
 }
 
--- manifests/site.pp
+++ manifests/site.pp
@@ -4,6 +4,7 @@ node /.*\.home\.brittg\.com/ {
 
 node 'linode.brittg.com' {
   class { 'profiles::base': }
+  class { 'profiles::webserver': }
 }
 
 node default {
