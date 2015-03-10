79b66d1fb416e4d8d099c4f963c42e72d75881f5
Britt Gresham <britt@puppetlabs.com>
2015-03-09 18:02:58 -0700

    Set module path while applying site.pp

--- manifests/base.pp
+++ manifests/base.pp
@@ -20,15 +20,18 @@ class profiles::base {
     remote => 'https://github.com/demophoon/puppet-environment.git',
   }
 
+  $apply_module_path = '/etc/puppet/environments/production/modules/'
+  $site_pp = "${apply_module_path}profiles/manifests/site.pp"
+
   cron { 'puppet apply':
-    command => '/usr/bin/puppet apply /etc/puppet/modules/profiles/manifests/site.pp',
-    user => 'root',
-    minute => ['0', '30'],
+    command => "/usr/bin/puppet apply --modulepath ${apply_module_path} ${site_pp}",
+    user    => 'root',
+    minute  => ['0', '30'],
   }
 
   cron { 'r10k update environments':
     command => '/usr/local/bin/r10k deploy environment -pv',
-    user => 'root',
-    minute => ['15', '45'],
+    user    => 'root',
+    minute  => ['15', '45'],
   }
 }
