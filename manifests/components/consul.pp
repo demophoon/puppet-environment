0ea09d3855876f07374f496d95c28e7c736450d7
Britt Gresham <brittcgresham@gmail.com>
2015-07-25 12:10:00 -0700

    Add server param to consul component

--- manifests/components/consul.pp
+++ manifests/components/consul.pp
@@ -1,16 +1,30 @@
 class profiles::components::consul(
   $datacenter,
+  $server = false,
+  $pool = undef,
 ){
-  class { '::consul':
-    config_hash => {
-      'bootstrap_expect' => 1,
-      'client_addr'      => '127.0.0.1',
-      'data_dir'         => '/opt/consul',
-      'datacenter'       => $datacenter,
-      'log_level'        => 'INFO',
-      'node_name'        => $::fqdn,
-      'server'           => true,
-      'ui_dir'           => '/opt/consul/ui',
+  if $server {
+    class { '::consul':
+      config_hash => {
+        'bootstrap_expect' => 1,
+        'client_addr'      => '127.0.0.1',
+        'data_dir'         => '/opt/consul',
+        'datacenter'       => $datacenter,
+        'log_level'        => 'INFO',
+        'node_name'        => $::fqdn,
+        'server'           => true,
+        'ui_dir'           => '/opt/consul/ui',
+      }
+    }
+  } else {
+    class { '::consul':
+      config_hash => {
+        'data_dir'         => '/opt/consul',
+        'datacenter'       => $datacenter,
+        'log_level'        => 'INFO',
+        'node_name'        => $::fqdn,
+        'retry_join'       => $pool,
+      }
     }
   }
 }
