f59d2baa608d1fdefe7609a7b3c1bf2e7cf636f4
Britt Gresham <brittcgresham@gmail.com>
2015-12-04 17:45:43 +0000

    Use full chain certificate instead of single cert

--- manifests/components/webserver/ssl_vhost.pp
+++ manifests/components/webserver/ssl_vhost.pp
@@ -3,11 +3,18 @@ define profiles::components::webserver::ssl_vhost (
   String $vhost_name = $title,
   String $letsencrypt_ssl_root = '/etc/letsencrypt/live',
   Boolean $rewrite_to_https = true,
+  Boolean $use_full_chain = true,
 ) {
+  if $use_full_chain {
+    $_certname = 'fullchain.pem'
+  } else {
+    $_certname = 'cert.pem'
+  }
+
   nginx::resource::vhost { "${vhost_name}":
     www_root         => "${www_root}",
     ssl              => true,
-    ssl_cert         => "${letsencrypt_ssl_root}/${vhost_name}/cert.pem",
+    ssl_cert         => "${letsencrypt_ssl_root}/${vhost_name}/${_certname}",
     ssl_key          => "${letsencrypt_ssl_root}/${vhost_name}/privkey.pem",
     rewrite_to_https => $rewrite_to_https,
   }
