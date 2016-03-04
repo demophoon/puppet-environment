393381836c2fd1490f945dcb6a3d0583c38a41b1
Britt Gresham <brittcgresham@gmail.com>
2016-03-04 19:04:24 +0000

    Consolidate certs into a single certificate

--- manifests/components/webserver.pp
+++ manifests/components/webserver.pp
@@ -16,6 +16,26 @@ class profiles::components::webserver {
           webroot     => '/var/www/brittg/',
           domain_name => 'www.brittg.com',
         },
+        {
+          webroot     => '/var/www/assets/',
+          domain_name => 'assets.brittg.com',
+        },
+        {
+          webroot     => '/var/www/',
+          domain_name => 'linode.brittg.com',
+        },
+        {
+          webroot     => '/var/www/htdocs/',
+          domain_name => 'htdocs.brittg.com',
+        },
+        {
+          webroot     => '/var/www/htdocs/brittbot/',
+          domain_name => 'brittbot.brittg.com',
+        },
+        {
+          webroot     => '/var/www/htdocs/music/',
+          domain_name => 'music.brittg.com',
+        },
       ]
     }
 
@@ -37,23 +57,53 @@ class profiles::components::webserver {
       require          => Letsencrypt_client::Cert["brittg.com"],
     }
 
+    nginx::resource::vhost { "assets.brittg.com":
+      www_root         => '/var/www/assets/',
+      ssl              => true,
+      ssl_cert         => "/etc/letsencrypt/live/brittg.com/fullchain.pem",
+      ssl_key          => "/etc/letsencrypt/live/brittg.com/privkey.pem",
+      rewrite_to_https => true,
+      require          => Letsencrypt_client::Cert["brittg.com"],
+    }
+
+    nginx::resource::vhost { "linode.brittg.com":
+      www_root         => '/var/www/',
+      ssl              => true,
+      ssl_cert         => "/etc/letsencrypt/live/brittg.com/fullchain.pem",
+      ssl_key          => "/etc/letsencrypt/live/brittg.com/privkey.pem",
+      rewrite_to_https => true,
+      require          => Letsencrypt_client::Cert["brittg.com"],
+    }
+
+    nginx::resource::vhost { "htdocs.brittg.com":
+      www_root         => '/var/www/htdocs',
+      ssl              => true,
+      ssl_cert         => "/etc/letsencrypt/live/brittg.com/fullchain.pem",
+      ssl_key          => "/etc/letsencrypt/live/brittg.com/privkey.pem",
+      rewrite_to_https => true,
+      require          => Letsencrypt_client::Cert["brittg.com"],
+    }
+
+    nginx::resource::vhost { "brittbot.brittg.com":
+      www_root         => '/var/www/htdocs/brittbot',
+      ssl              => true,
+      ssl_cert         => "/etc/letsencrypt/live/brittg.com/fullchain.pem",
+      ssl_key          => "/etc/letsencrypt/live/brittg.com/privkey.pem",
+      rewrite_to_https => true,
+      require          => Letsencrypt_client::Cert["brittg.com"],
+    }
+
+    nginx::resource::vhost { "music.brittg.com":
+      www_root         => '/var/www/htdocs/music',
+      ssl              => true,
+      ssl_cert         => "/etc/letsencrypt/live/brittg.com/fullchain.pem",
+      ssl_key          => "/etc/letsencrypt/live/brittg.com/privkey.pem",
+      rewrite_to_https => true,
+      require          => Letsencrypt_client::Cert["brittg.com"],
+    }
+
     nginx::resource::vhost { 'cards.brittg.com':
       proxy => 'http://localhost:3143',
-    }
-    profiles::components::webserver::ssl_vhost { 'assets.brittg.com':
-      www_root => '/var/www/assets/',
-    }
-    nginx::resource::vhost { 'linode.brittg.com':
-      www_root => '/var/www/',
-    }
-    nginx::resource::vhost { 'htdocs.brittg.com':
-      www_root => '/var/www/htdocs/',
-    }
-    nginx::resource::vhost { 'music.brittg.com':
-      www_root => '/var/www/htdocs/music/',
-    }
-    profiles::components::webserver::ssl_vhost { 'brittbot.brittg.com':
-      www_root => '/var/www/htdocs/brittbot/',
     }
     nginx::resource::vhost { 'reader.brittg.com':
       proxy => 'http://localhost:8082',
