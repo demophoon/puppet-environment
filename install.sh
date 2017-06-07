d5b6dc0aa3666d5584499e0b1aeb8ecef6953361
Britt Gresham <britt@brittg.com>
2017-06-07 05:20:19 +0000

    Only install puppet if new node

--- install.sh
+++ install.sh
@@ -1,29 +1,34 @@
 #!/bin/bash
 
-command_exists () {
+command_exists() {
     type "$1" &> /dev/null ;
 }
 
-if command_exists dpkg; then
-    source /etc/os-release
+install_puppet() {
+    if command_exists dpkg; then
+        source /etc/os-release
+        release_file="puppetlabs-release-pc1-${UBUNTU_CODENAME:?}.deb"
+        wget "https://apt.puppetlabs.com/${release_file:?}"
+        sudo dpkg -i ${release_file:?}
+        rm ${release_file:?}
+        sudo apt-get update
+        sudo apt-get install puppet-agent -y
+    elif command_exists yum; then
+        source /etc/os-release
+        release_file="puppetlabs-release-pc1-el-${VERSION_ID:?}.noarch.rpm"
+        sudo rpm -Uvh "https://yum.puppetlabs.com/${release_file:?}"
+        sudo yum install puppet-agent -y
+    fi
+}
 
-    release_file="puppetlabs-release-pc1-${UBUNTU_CODENAME:?}.deb"
-    wget "https://apt.puppetlabs.com/${release_file:?}"
-    sudo dpkg -i ${release_file:?}
-    rm ${release_file:?}
-    sudo apt-get update
-    sudo apt-get install puppet-agent -y
-elif command_exists yum; then
-    source /etc/os-release
-    release_file="puppetlabs-release-pc1-el-${VERSION_ID:?}.noarch.rpm"
-    sudo rpm -Uvh "https://yum.puppetlabs.com/${release_file:?}"
-    sudo yum install puppet-agent -y
+if ! command_exists /opt/puppetlabs/puppet/bin/puppet; then
+    install_puppet
 fi
 
 puppet="/opt/puppetlabs/puppet/bin/puppet"
 r10k="/opt/puppetlabs/puppet/bin/r10k"
 
-sudo ${puppet:?} module install puppet/r10k
+sudo ${puppet:?} module install puppet/r10k --force
 sudo ${puppet:?} apply -e "\
 class { 'r10k':\
   sources => {\
