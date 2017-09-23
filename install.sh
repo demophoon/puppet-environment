b7107d9ad072441178a67daccddec3e31b9524c2
Britt Gresham <britt@brittg.com>
2017-09-22 20:40:22 -0700

    Update bash script to setup hiera on a server

--- install.sh
+++ install.sh
@@ -5,39 +5,93 @@ command_exists() {
 }
 
 install_puppet() {
+    # Currently only supports ubuntu
     if command_exists dpkg; then
         source /etc/os-release
-        release_file="puppetlabs-release-pc1-${UBUNTU_CODENAME:?}.deb"
+        release_file="puppet5-release-${UBUNTU_CODENAME:?}.deb"
         wget "https://apt.puppetlabs.com/${release_file:?}"
-        sudo dpkg -i ${release_file:?}
+        dpkg -i ${release_file:?}
         rm ${release_file:?}
-        sudo apt-get update
-        sudo apt-get install puppet-agent -y
+        apt-get update
+        apt-get install puppet-agent -y
     elif command_exists yum; then
         source /etc/os-release
-        release_file="puppetlabs-release-pc1-el-${VERSION_ID:?}.noarch.rpm"
-        sudo rpm -Uvh "https://yum.puppetlabs.com/${release_file:?}"
-        sudo yum install puppet-agent -y
+        release_file="puppet5-release-el-${VERSION_ID:?}.noarch.rpm"
+        rpm -Uvh "https://yum.puppetlabs.com/${release_file:?}"
+        yum install puppet-agent -y
+    else
+      echo "Unsupported platform :("
+      exit 1
     fi
 }
+
+confirm() {
+  read -p '> ' response
+  echo
+  if [[ ${response} =~ [yY] ]]; then
+    return 0
+  fi
+  return 1
+}
+
+if [ ${UID} -ne 0 ]; then
+  echo "This script must run as root to work properly."
+  exit 1
+fi
 
 if ! command_exists /opt/puppetlabs/puppet/bin/puppet; then
     install_puppet
 fi
 
+echo "Would you like to setup access to your hiera data repo? [y/N]"
+echo "Warning: Only answer yes to this if you are Demophoon or have access to the private hiera data repo!"
+
+if confirm; then
+  if [ -ne /root/.ssh/id_rsa.pub ]; then
+    echo "We couldn't find a ssh key in '/root/.ssh/'."
+    echo "Would you like us to generate one? [y/N]"
+    echo "Warning: Answering no may cause the install script to fail!"
+    if confirm; then
+      ssh-keygen -b 4096 -t rsa -f '/root/.ssh/id_rsa'
+      echo
+    fi
+  fi
+  echo "Add the following public key to the hiera data repo's deploy keys."
+  echo
+  cat /root/.ssh/id_rsa.pub
+  echo
+  echo "Press enter to continue."
+  confirm
+  use_hiera=0
+fi
+
 puppet="/opt/puppetlabs/puppet/bin/puppet"
 r10k="/opt/puppetlabs/puppet/bin/r10k"
 
-sudo ${puppet:?} module install puppet/r10k --force
-sudo ${puppet:?} apply -e "\
-class { 'r10k':\
-  sources => {\
-    'default' => {\
-      'remote'  => 'https://github.com/demophoon/puppet-environment.git',\
-      'basedir' => '/etc/puppetlabs/code/environments',\
-    },\
-  },\
+${puppet:?} module install puppet/r10k --force
+
+hiera_sources=""
+if $(exit ${use_hiera:-1}); then
+  hiera_sources="
+'hiera' => {
+  'remote'       => 'git@github.com:demophoon/hieradata.git',
+  'prefix'       => true,
+  'git_settings' => {
+    'private_key' => '/root/.ssh/id_rsa',
+  },
+"
+fi
+
+${puppet:?} apply -e "
+class { 'r10k':
+  sources => {
+    'puppet' => {
+      'remote' => 'https://github.com/demophoon/puppet-environment.git',
+      'prefix' => false,
+    },
+    ${hiera_sources}
+  },
 }"
 
-sudo ${r10k:?} deploy environment -pv
-sudo ${puppet:?} apply --modulepath /etc/puppetlabs/code/environments/production/modules/ /etc/puppetlabs/code/environments/production/site.pp
+${r10k:?} deploy environment -pv
+${puppet:?} apply --modulepath /etc/puppetlabs/code/environments/production/modules/ /etc/puppetlabs/code/environments/production/site.pp
