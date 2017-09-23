177c70ac6009912cf729ca325441e044a148e825
Britt Gresham <britt@brittg.com>
2017-09-23 00:57:02 -0700

    Add closure_scope to get current evaluating scope
    ...its getting late and i'm missing simple things now.
    
    ...maybe i should stop.

--- lib/puppet/functions/enable_private_hiera.rb
+++ lib/puppet/functions/enable_private_hiera.rb
@@ -1,5 +1,6 @@
 Puppet::Functions.create_function(:'enable_private_hiera') do
   def enable_private_hiera()
+    scope = closure_scope
     environment = scope['environment']
     [
       "/etc/puppetlabs/puppet/hiera/#{environment}",
