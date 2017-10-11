e70ae8757bb3663b3d2026edc2ff2586965d1f7c
Britt Gresham <britt@brittg.com>
2017-10-10 18:14:35 -0700

    Fix mount fstype typo

--- manifests/components/plex/mounts.pp
+++ manifests/components/plex/mounts.pp
@@ -2,7 +2,7 @@ class profiles::components::plex::mounts {
   mount { '/media/tb-tb':
     ensure   => mounted,
     device   => '/dev/sda1',
-    fetype   => 'ext4',
+    fstype   => 'ext4',
     options  => 'defaults',
     remounts => true,
     atboot   => true,
