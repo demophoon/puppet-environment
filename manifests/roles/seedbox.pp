112a4a4232fd8cbf1799d6abdf1851e6b983a7a7
Britt Gresham <britt@brittg.com>
2017-10-29 02:01:59 -0700

    Add missing transmission params

--- manifests/roles/seedbox.pp
+++ manifests/roles/seedbox.pp
@@ -1,3 +1,9 @@
 class profiles::roles::seedbox () {
-  include transmission
+  class { 'transmission':
+    rpc_username => 'britt',
+    rpc_password => 'password',
+    rpc_port     => 8080,
+    peer_port    => 54612,
+    encryption   => 2,
+  }
 }
