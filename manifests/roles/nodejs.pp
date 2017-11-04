172c7db52a745da7b37174a1963be6bde12c158d
Britt Gresham <britt@brittg.com>
2017-11-04 12:09:45 -0700

    Add node symlink

--- manifests/roles/nodejs.pp
+++ manifests/roles/nodejs.pp
@@ -4,4 +4,16 @@ class profiles::roles::nodejs () {
     nodejs_dev_package_ensure => 'present',
     npm_package_ensure        => 'present',
   }
+
+  package { 'grunt-cli':
+    ensure   => 'present',
+    provider => 'npm',
+    require  => Class['::nodejs'],
+  }
+
+  file { '/usr/bin/node':
+    ensure  => link,
+    target  => '/usr/bin/nodejs',
+    require => Class['::nodejs'],
+  }
 }
