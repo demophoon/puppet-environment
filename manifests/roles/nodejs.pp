952fe14781da0b5f086e450f6d2122e120ac3ee1
Britt Gresham <britt@brittg.com>
2017-11-04 12:02:24 -0700

    Ubuntu needs this because reasons.

--- manifests/roles/nodejs.pp
+++ manifests/roles/nodejs.pp
@@ -1,3 +1,7 @@
 class profiles::roles::nodejs () {
-  class { 'nodejs': }
+  class { '::nodejs':
+    manage_package_repo       => false,
+    nodejs_dev_package_ensure => 'present',
+    npm_package_ensure        => 'present',
+  }
 }
