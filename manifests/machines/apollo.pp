b19698b9a2e7743bbd62f533708f8635b586bed0
Britt Gresham <britt@brittg.com>
2017-12-21 16:35:58 -0800

    Use vagrant role instead of module

--- manifests/machines/apollo.pp
+++ manifests/machines/apollo.pp
@@ -1,12 +1,12 @@
 class profiles::machines::apollo (){
   include docker
-  include vagrant
 
   include profiles::roles::development
   include profiles::roles::bolt
 
   include profiles::roles::apps
   include profiles::roles::yubikey
+  include profiles::roles::vagrant
 
   profiles::roles::backup { $::fqdn:
     backup_dirs => [
