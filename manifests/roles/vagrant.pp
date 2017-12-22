78aab73128182057bcdb6cd51324cd8470316537
Britt Gresham <britt@brittg.com>
2017-12-21 16:28:29 -0800

    Ensure virtualbox is available for vagrant to work properly

--- manifests/roles/vagrant.pp
+++ manifests/roles/vagrant.pp
@@ -1,4 +1,13 @@
 class profiles::roles::vagrant () {
   include vagrant
+
+  apt::key { 'oracle':
+    id     => 'B9F8D658297AF3EFC18D5CDFA2F683C52980AECF',
+    server => 'pgp.mit.edu',
+    notify => Exec['apt_update'],
+   } ->
+  package { 'virtualbox-5.2':
+    ensure => present,
+  }
 }
 
