40b18b8283d896a2ee13054253b7126d6bc99217
Britt Gresham <britt@brittg.com>
2018-01-07 08:37:13 -0800

    Use docker-compose to start owncloud

--- manifests/roles/docker/owncloud.pp
+++ manifests/roles/docker/owncloud.pp
@@ -1,7 +1,24 @@
-class profiles::roles::docker::owncloud () {
-  exec {'start_owncloud':
-    command => "/usr/bin/docker run -p 9263:80 -v /media/tb-tb/owncloud/:/var/www/html:rw -itd owncloud",
-    unless  => "/usr/bin/docker ps | /bin/grep -q owncloud",
-    path    => '/usr/bin:/bin',
+class profiles::roles::docker::owncloud (
+  $db_password = undef,
+) {
+  if ($db_password == undef) {
+    warning('Unable to setup owncloud. Please set database password.')
+  } else {
+    file { '/tmp/owncloud.docker/':
+      ensure => 'directory',
+      owner  => 'root',
+      mode   => '600',
+    }
+    file { '/tmp/owncloud.docker/docker-compose.yml':
+      ensure  => 'present',
+      owner   => 'root',
+      mode    => '600',
+      content => template('profiles/owncloud-docker-compose.yml.erb')
+    }
+
+    docker_compose { '/tmp/owncloud.docker/docker-compose.yml':
+      ensure  => present,
+      require => File['/tmp/owncloud.docker/docker-compose.yml'],
+    }
   }
 }
