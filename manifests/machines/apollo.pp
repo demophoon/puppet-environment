f59958766a27a2c920a2914b509693dbe9aad447
Britt Gresham <britt@brittg.com>
2018-01-14 13:11:29 -0800

    Classify apollo with media client

--- manifests/machines/apollo.pp
+++ manifests/machines/apollo.pp
@@ -7,6 +7,8 @@ class profiles::machines::apollo (){
   include profiles::roles::vagrant
   include profiles::roles::i3
   include profiles::roles::docker
+  include profiles::roles::nfs::client
+  include profiles::roles::media::client
 
   class { 'profiles::roles::backup':
     backup_dirs => [
--- manifests/machines/beryllium.pp
+++ manifests/machines/beryllium.pp
@@ -9,7 +9,7 @@ class profiles::machines::beryllium (){
   include profiles::roles::docker
   include profiles::roles::zfs
   include profiles::roles::samba::server
-  include profiles::roles::media_server
+  include profiles::roles::media::server
 
   include profiles::roles::seedbox
 
