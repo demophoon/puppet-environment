90fcac36afe556746613fa7bbad198eaaff6cc0e
Britt Gresham <britt@brittg.com>
2018-01-22 13:48:58 -0800

    Allow options to be passed into docker

--- manifests/roles/docker.pp
+++ manifests/roles/docker.pp
@@ -1,5 +1,9 @@
-class profiles::roles::docker () {
-  class { 'docker': }
+class profiles::roles::docker (
+  $options = undef,
+) {
+  class { 'docker':
+    * => $options,
+  }
   class { 'docker::compose':
     ensure  => present,
     version => '1.18.0',
