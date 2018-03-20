dc7c72179f8f6b328cbd9dc5c3c8a35601e6ba70
Britt Gresham <britt@brittg.com>
2018-03-20 22:31:23 +0000

    Export projects directory

--- manifests/machines/work/blackwell.pp
+++ manifests/machines/work/blackwell.pp
@@ -1,5 +1,14 @@
-class profiles::machines::work::blackwell () {
+class profiles::machines::work::blackwell (
+  $nfs_clients = 'localhost(rw)',
+) {
   include profiles::roles::development
   include profiles::roles::docker
   include profiles::roles::sudoers
+
+  include profiles::roles::nfs::server
+
+  nfs::server::export {'/home/britt/projects':
+    ensure  => present,
+    clients => $nfs_clients,
+  }
 }
