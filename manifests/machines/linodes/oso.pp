e009d0c01835b0a428c3fdff40ef089b78e80d11
Britt Gresham <britt@brittg.com>
2018-04-19 13:15:44 -0700

    Remove non-functional classes from oso to prepare for decom

--- manifests/machines/linodes/oso.pp
+++ manifests/machines/linodes/oso.pp
@@ -1,10 +1,6 @@
 class profiles::machines::linodes::oso {
   require profiles::machines::linode
 
-  include profiles::roles::docker
-  include profiles::roles::nfs::client
-  include profiles::roles::media::client
-
   User <| tag == 'tx.dc' |>
   Ssh_authorized_key <| tag == 'tx.dc' |>
 
