4479db280dcbe35c9b262e80870008216db3a2bc
Britt Gresham <britt@brittg.com>
2018-08-30 15:43:24 -0700

    :package: Split out debian packages from other supported oses.

--- manifests/components/packages.pp
+++ manifests/components/packages.pp
@@ -1,33 +1,44 @@
 class profiles::components::packages {
-  $global_packages = [
-    'vim',
-    'zsh',
+  $_debian_packages = [
     'bash',
     'ntp',
     'p7zip',
     'unzip',
+    'curl',
+    'htop',
+    'silversearcher-ag',
+    'fail2ban',
+  ]
+
+  $_osx_packages = [
+    'gnu-tar',
+    'the_silver_searcher',
+  ]
+
+  $_global_packages = [
+    'zsh',
+    'vim',
     'tmux',
     'mercurial',
     'tree',
-    'curl',
     'wget',
-    'htop',
-    'silversearcher-ag',
   ]
 
-  $webserver_packages = [
-      'fail2ban',
-  ]
+  case $::osfamily {
+    /Darwin/: {
+      $install_packages = $_global_packages + $_osx_packages
+    }
+    /Debian/: {
+      $install_packages = $_global_packages + $_debian_packages
+    }
+    default: {
+      $install_packages = $_global_packages
+    }
+  }
 
-  @package { $global_packages:
+  @package { $install_packages:
     ensure => latest,
     tag    => ['global'],
   }
-
-  @package { $webserver_packages:
-    ensure => latest,
-    tag    => ['webserver'],
-  }
-
 }
 
