65bc506fd51e1eb0c4ef3fc5077c36f57405b1fa
Britt Gresham <britt@brittg.com>
2018-08-31 16:32:11 -0700

    :apple: Enable masterless role on osx

--- manifests/base.pp
+++ manifests/base.pp
@@ -1,4 +1,27 @@
-class profiles::base () inherits profiles::params {
+class profiles::base (
+  Hash    $additional_r10k_sources = {},
+  String  $hiera_private_key       = '/root/.ssh/id_rsa',
+) inherits profiles::params {
+
+  include profiles::roles::masterless
+
+  # We need to make sure we have a key so we at least have a chance at authenticating against the repo
+  if enable_private_hiera() {
+    class { 'hiera':
+      hiera_version  => '5',
+      hiera5_defaults => {
+        'datadir'   => "${::settings::confdir}/hiera/master/",
+        'data_hash' => 'yaml_data',
+      },
+      hierarchy      => [
+        { 'name' => 'Node', 'path' => 'nodes/%{fqdn}.yaml' },
+        { 'name' => 'Environment', 'path' => 'environments/%{environment}.yaml' },
+        { 'name' => 'Global', 'path' => 'global.yaml' },
+      ],
+    }
+  }
+
+  ## Os Specific Bindings
   case $::osfamily {
     /Debian/: {
       include profiles::roles::linux
