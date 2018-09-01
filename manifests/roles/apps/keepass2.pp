2dc6b0f638107ee8bdde9c944e98be0031dbe012
Britt Gresham <britt@brittg.com>
2018-08-31 19:46:20 -0700

    :package: Allow keepass to be installed on osx

--- manifests/roles/apps/keepass2.pp
+++ manifests/roles/apps/keepass2.pp
@@ -1,5 +1,9 @@
 class profiles::roles::apps::keepass2 () {
-  package { 'keepass2':
+  case $::osfamily {
+    'Darwin': { $keepass2 = 'keepassxc' }
+    default: { $keepass2 = 'keepass2' }
+  }
+  package { $keepass2:
     ensure => 'present',
   }
 }
