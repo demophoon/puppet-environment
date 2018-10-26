5087c8dccefa596a4843ced714b8c0a29441c5d2
Britt Gresham <britt@brittg.com>
2018-10-25 17:38:11 -0700

    :package: Install autotype package for keypass

--- manifests/roles/apps/keepass2.pp
+++ manifests/roles/apps/keepass2.pp
@@ -3,5 +3,19 @@ class profiles::roles::apps::keepass2 () {
     'Darwin': { $keepass2 = 'keepassxc' }
     default: { $keepass2 = 'keepass2' }
   }
+
+  case $::osfamily {
+    'Debian': {
+      case $facts['os']['release']['major'] {
+        '18.04': { $additional_packages = ['xdotool'] }
+        default: { $additional_packages = [] }
+      }
+    }
+    default: {
+      $additional_packages = []
+    }
+  }
+
   package { $keepass2: }
+  package { $additional_packages: }
 }
