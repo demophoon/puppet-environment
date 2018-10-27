f8c3e71c8a0689ec7e1eb5ee37c8944df7927276
Britt Gresham <britt@brittg.com>
2018-10-27 00:55:41 -0700

    Add gesture support to apollo

--- manifests/machines/apollo.pp
+++ manifests/machines/apollo.pp
@@ -28,9 +28,28 @@ class profiles::machines::apollo (){
   package { [
     'cmus',
     'emacs25',
-    'xserver-xorg-input-synaptics',
   ]:
     ensure  => present,
     require => Apt::Ppa['ppa:ubuntu-elisp/ppa'],
+  }
+
+  package { [
+    # Multitouch Gesture Support
+    'xserver-xorg-input-libinput',
+    'wmctrl',
+    'libinput-tools',
+    ]:
+      ensure => 'present',
+  } ->
+  vcsrepo { '/opt/libinput-gestures':
+    ensure   => 'present',
+    provider => 'git',
+    source   => 'https://github.com/bulletmark/libinput-gestures.git',
+  } ~>
+  exec { 'Install libinput-gestures':
+    command     => 'libinput-gestures-setup install',
+    path        => '/bin:/usr/bin:/usr/local/bin:/opt/libinput-gestures',
+    cwd         => '/opt/libinput-gestures',
+    refreshonly => true,
   }
 }
