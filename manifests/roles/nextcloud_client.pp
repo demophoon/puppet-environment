0f60d63c670a7306b416cca10698f06c78054f7a
Britt Gresham <britt@brittg.com>
2018-11-01 14:29:12 -0700

    Add dependency chains to nextcloud client install

--- manifests/roles/nextcloud_client.pp
+++ manifests/roles/nextcloud_client.pp
@@ -15,11 +15,11 @@ class profiles::roles::nextcloud_client (
       apt::key { 'nextcloud':
         id     => '1FCD77DD0DBEF5699AD2610160EE47FBAD3DD469',
         server => 'keyserver.ubuntu.com',
-      }
+      } ->
       apt::ppa { 'ppa:nextcloud-devs/client':
         require => Apt::Key['nextcloud'],
         notify  => Exec['apt_update'],
-      }
+      } ->
       package { 'nextcloud-client':
         ensure  => latest,
         require => [Apt::Ppa['ppa:nextcloud-devs/client'], Exec['apt_update']],
