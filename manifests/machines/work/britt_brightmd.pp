3b2caf9c0cd60e0d090033e4651c3f65b5b88250
Britt Gresham <britt@brittg.com>
2018-11-01 13:43:05 -0700

    :memo: Add classification for work laptop

---
+++ manifests/machines/work/britt_brightmd.pp
@@ -0,0 +1,52 @@
+class profiles::machines::work::britt_brightmd (
+  $username = 'britt',
+) {
+  include profiles::roles::apps
+  include profiles::roles::apps::slack
+  include profiles::roles::apps::osx
+  #include infra_private::roles::aorta
+
+  Package {
+    ensure   => present,
+  }
+
+  Exec {
+    path        => '/bin:/usr/bin:/usr/local/bin',
+    user        => $username,
+    environment => ["HOME=/home/${username}"],
+    refreshonly => true,
+  }
+
+  File {
+    owner => $username,
+    group => $username,
+  }
+
+ package { 'nvim': } ->
+ package { 'neovim':
+   ensure   => latest,
+   provider => 'pip',
+ } ->
+ package { 'neovim':
+   ensure   => latest,
+   provider => 'pip3',
+ }
+
+  package { [
+    'awscli',
+    'vagrant',
+    'virtualbox',
+  ]: }
+
+  file { '/etc/letsencrypt':
+    ensure => directory,
+  }
+
+  package { 'gettext': }
+  package { 'mongodb-org': }
+
+  package { ['magic-wormhole', 'virtualenvwrapper']:
+    ensure   => latest,
+    provider => 'pip',
+  }
+}
