7398384eef88f721baeaed918a5a44bfbe238973
Britt Gresham <britt@brittg.com>
2018-11-02 06:34:13 -0700

    :fire: Remove old infra_private refs

--- manifests/machines/work/britt_brightmd.pp
+++ manifests/machines/work/britt_brightmd.pp
@@ -12,8 +12,6 @@ class profiles::machines::work::britt_brightmd (
   include profiles::roles::linux::thinkpad
   include profiles::roles::docker
 
-  #include infra_private::roles::aorta
-
   Exec {
     path        => '/bin:/usr/bin:/usr/local/bin',
     user        => $username,
