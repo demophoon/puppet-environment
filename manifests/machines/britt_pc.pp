1856452368b9eec5996ca4a86f7f0cb3263bc653
Britt Gresham <britt@brittg.com>
2019-02-03 14:41:19 -0800

    Add i3 packages

--- manifests/machines/britt_pc.pp
+++ manifests/machines/britt_pc.pp
@@ -1,3 +1,29 @@
 class profiles::machines::britt_pc () {
+  include apt
+
   include profiles::roles::britt
+  include profiles::roles::nextcloud_client
+  include profiles::roles::apps
+  include profiles::roles::i3
+
+  package { [
+    'weechat',
+    'xscreensaver',
+    'xscreensaver-data',
+    'xscreensaver-gl',
+    'xscreensaver-data-extra',
+    'xscreensaver-gl-extra',
+  ]:
+    ensure => 'latest',
+  }
+
+  apt::ppa { 'ppa:ubuntu-elisp/ppa': }
+
+  package { [
+    'cmus',
+    'emacs25',
+  ]:
+    ensure  => present,
+    require => Apt::Ppa['ppa:ubuntu-elisp/ppa'],
+  }
 }
