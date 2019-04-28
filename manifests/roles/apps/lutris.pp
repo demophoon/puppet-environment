c1d081cd693c2e55c5aec63192e0752bce093ee8
Britt Gresham <britt@brittg.com>
2019-04-28 09:00:15 -0700

    Add missing drivers

--- manifests/roles/apps/lutris.pp
+++ manifests/roles/apps/lutris.pp
@@ -8,6 +8,18 @@ class profiles::roles::apps::lutris () {
       package { 'lutris':
         ensure => $ensure,
       }
+
+      apt::ppa { 'ppa:graphics-drivers/ppa':
+        notify  => Exec['apt_update'],
+      } ->
+      package { 'nvidia-driver-418': }
+
+      package { [
+          'libvulkan1',
+          'libvulkan1:i386',
+        ]:
+      }
+
     }
     default: {
       notify { "Lutris is not avaliable on ${::osfamily}": }
