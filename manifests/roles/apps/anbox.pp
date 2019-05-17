880976b9a3c9c485c6c3b526c894e5a9af25f1d0
Britt Gresham <britt@brittg.com>
2019-05-17 09:09:40 -0700

    Add anbox role

---
+++ manifests/roles/apps/anbox.pp
@@ -0,0 +1,13 @@
+class profiles::roles::apps::anbox () {
+  include ::snapd
+
+  apt::ppa { 'ppa:morphis/anbox-support': } ->
+  package { ['anbox-modules-dkms']:
+    ensure => present,
+  } ->
+  package { 'anbox':
+    ensure   => latest,
+    provider => 'snap',
+  }
+
+}
