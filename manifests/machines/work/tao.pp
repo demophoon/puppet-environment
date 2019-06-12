4869e73df909168145925368dd2db875bd18d1db
Britt Gresham <britt@brittg.com>
2019-06-12 08:37:12 -0700

    Add circleci laptop to manifests

---
+++ manifests/machines/work/tao.pp
@@ -0,0 +1,30 @@
+class profiles::machines::apollo (){
+  include profiles::roles::development
+  include profiles::roles::wifi
+  include profiles::roles::ansible
+  include profiles::roles::nextcloud_client
+  include profiles::roles::apps::slack
+  include profiles::roles::apps::vscode
+  include profiles::roles::development::hub
+
+  include profiles::roles::apps
+  include profiles::roles::apps::light
+  include profiles::roles::yubikey
+  include profiles::roles::vagrant
+  include profiles::roles::i3
+  include profiles::roles::linux::thinkpad
+  include profiles::roles::docker
+
+  apt::ppa { 'ppa:ubuntu-elisp/ppa': }
+
+  package { [
+    'cmus',
+    'emacs25',
+  ]:
+    ensure  => present,
+    require => Apt::Ppa['ppa:ubuntu-elisp/ppa'],
+  }
+
+  package { 'neovim':
+  }
+}
