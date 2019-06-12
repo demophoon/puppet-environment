781ac613267d797d328425837ad6a4c150279e81
Britt Gresham <britt@brittg.com>
2019-06-12 13:15:19 -0700

    Add nextcloud client to tao

--- manifests/components/nextcloud_client/init.pp
+++ manifests/components/nextcloud_client/init.pp
@@ -1,19 +1,19 @@
-class profiles::components::nextcloud_client (
+class profiles::components::nextcloud_client::init (
   $user       = 'britt',
   $config_dir = "/home/${user}/.local/share/data/ownCloud",
 ) {
-  apt::key { 'nextcloud':
-    id     => '1FCD77DD0DBEF5699AD2610160EE47FBAD3DD469',
-    server => 'keyserver.ubuntu.com',
-  }
-  apt::ppa { 'ppa:nextcloud-devs/client':
-    require => Apt::Key['nextcloud'],
-    notify  => Exec['apt_update'],
-  }
-  package { 'nextcloud-client':
-    ensure  => latest,
-    require => [Apt::Ppa['ppa:nextcloud-devs/client'], Exec['apt_update']],
-  }
+  #apt::key { 'nextcloud':
+  #  id     => '1FCD77DD0DBEF5699AD2610160EE47FBAD3DD469',
+  #  server => 'keyserver.ubuntu.com',
+  #}
+  #apt::ppa { 'ppa:nextcloud-devs/client':
+  #  require => Apt::Key['nextcloud'],
+  #  notify  => Exec['apt_update'],
+  #}
+  #package { 'nextcloud-client':
+  #  ensure  => latest,
+  #  require => [Apt::Ppa['ppa:nextcloud-devs/client'], Exec['apt_update']],
+  #}
   # file { split($config_dir, '/'):
   #   ensure => directory,
   # }
