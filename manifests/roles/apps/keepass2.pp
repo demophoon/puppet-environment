f22a149084cabe5e0be05eead54e13ddbfc07ec4
Britt Gresham <britt@brittg.com>
2019-06-25 10:56:23 -0700

    Replace keepass2 with keepassxc

--- manifests/roles/apps/keepass2.pp
+++ manifests/roles/apps/keepass2.pp
@@ -1,7 +1,12 @@
 class profiles::roles::apps::keepass2 () {
   case $::osfamily {
     'Darwin': { $keepass2 = 'keepassxc' }
-    default: { $keepass2 = 'keepass2' }
+    default: {
+      $keepass2 = 'keepassxc'
+      package { 'keepass2':
+        ensure => absent,
+      }
+    }
   }
 
   case $::osfamily {
