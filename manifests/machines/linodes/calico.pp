4944f068488fffeb19cd4861497d7c40709c540e
Britt Gresham <britt@brittg.com>
2019-07-09 15:59:56 -0700

    Remove nginx and matrix

--- manifests/machines/linodes/calico.pp
+++ manifests/machines/linodes/calico.pp
@@ -6,23 +6,7 @@ class profiles::machines::linodes::calico {
   include profiles::roles::docker::znc
   include profiles::roles::docker::ttrss
 
-  include profiles::roles::nginx
-
-  include profiles::roles::matrix
-
   include profiles::roles::nfs::client
-
-  nginx::resource::server { ['mastodon-test.calico.demophoon.com']:
-    proxy => 'https://localhost:3000',
-  }
-
-  nginx::resource::server { ['reader.brittg.com', 'reader.calico.demophoon.com']:
-    proxy => 'http://localhost:5422',
-  }
-
-  nginx::resource::server { ['test.calico.demophoon.com']:
-    proxy => 'http://localhost:1234',
-  }
 
   class { 'profiles::roles::backup':
     backup_dirs => [
