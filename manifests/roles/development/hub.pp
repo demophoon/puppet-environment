4c701046384a728110265a3bf5efa5dad6dac1fe
Britt Gresham <britt@brittg.com>
2019-07-10 11:03:27 -0700

    Allow hub on all hosts

--- manifests/roles/development/hub.pp
+++ manifests/roles/development/hub.pp
@@ -1,9 +1,7 @@
 class profiles::roles::development::hub () {
-  if $::facts['os']['name'] == 'Ubuntu' {
-    include ::snapd
-    package { 'hub':
-      ensure   => latest,
-      provider => 'snap',
-    }
+  include ::snapd
+  package { 'hub':
+    ensure   => latest,
+    provider => 'snap',
   }
 }
