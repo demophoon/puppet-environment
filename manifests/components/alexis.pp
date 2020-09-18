0cc0723c8ec92c9389bc7076d04ba0a0bf82fe2c
Alexis <alexis@Alexiss-MBP.cascadia.demophoon.com>
2020-09-18 10:12:52 -0700

    replaced old ssh key and user

--- manifests/components/alexis.pp
+++ manifests/components/alexis.pp
@@ -1,27 +1,17 @@
 class profiles::components::users {
   # Users
-  @user { 'fgresham':
+  user { 'alexis':
     ensure         => present,
-    home           => '/home/fgresham',
+    home           => '/home/alexis',
     purge_ssh_keys => true,
     managehome     => true,
-    tag            => ['tx.dc'],
   }
 
   # Authorized SSH Keys
-  @ssh_authorized_key { 'fgresham@Freds-MacBook-Pro.local':
-    user    => 'fgresham',
+  ssh_authorized_key { 'alexis@Alexiss-MBP.cascadia.demophoon.com':
+    user    => 'alexis',
     type    => 'ssh-rsa',
-    key     => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCi9tCkE3ydnxeSthLcEICPhAX91HpX3tdzaYva5SNbzr/OjghCa5QW3FFH0rL9IeHWsu0hXCL9qjtdqtdSNFoPNtjez1buwu78nNCFC+iSqNZIZ0W7z93FMeE7cDWV3VoUBrHv6fWCNdDUeabSdeXJTE004My7GuJhRbCS/LQQkoyg3cGSBdtBcx+nuV5Qek/YFqj7L3pMTaNTCL8//j5fMbJENc7BWDifIoENSLCF4mMYXDitc4TOSP8ycgaekWACxnpCTmgkReXbi987h09tNFpksrvu5jJLeqdr3ZDb3wqlOJLV5FNtRVndh36K6MsuW1q68fwbCkTcIoAA9AW/',
-    tag     => ['tx.dc'],
-    require => User['fgresham'],
-  }
-
-  @ssh_authorized_key { 'agent@agents-MBP-2.home':
-    user    => 'fgresham',
-    type    => 'ssh-rsa',
-    key     => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQC9AW476KW4MqsFE/oJ08rm20IGD5x2BjOgaHo5XsnLrtznvl6VM/SSqshVg7nPwCHAYSlir2gPz6QroFyRoDxhheJ0OSTLW/8sGsjUr1RxdhTHn8oZRVoijn06o9zsNlZfRqIyl8FlJlaJHuU1ySvkInC1iCxR8SDV/1S1nLP6TrnZgfauAYK/zB+T/0s8e6uH6/AHCu0f0an0Lr+S5QN/KdJMiBdNX950T8tq0ZjwxkrPzD/uO1VD6qDB9V7a8L3E2nnYrcxRVb5s7JWRR3iYZoe3IPZZbMT3cWwie2OODaP5m1JruAFE9NGSIavLD/XnQSXSyjmZg9bqW73teo1X',
-    tag     => ['tx.dc'],
-    require => User['fgresham'],
+    key     => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQC0KPW9JSl7bcBaAqIMa9Wxbw6qo5gctFqkP5RCgw+7N9D4dpU0dymD90QTq8cDs8oEDiJkngRPmvs9x6ctx5OJgMLHcPKFgmjnOD947kmNZyog11rIeyP3Z9upSS5L5dHILA7OC5JYSoddNO2PM7q0rs4yu1zfb62hx/i7rN3LVv3Qr/9RX3pmlRz+ew3k07fp9nciDfhzPgKX7keunhWRl/ZIPUV8WtOPdb17bOaWAiPki4Z9eilAww4v5bumW8dfdo+DmNF1O6N74+g5yVrBjFCOnHWp+nUWVeA6No81x2zOcURClYJvU60dzjwytq8JML8GntaPFQpRHrq3b4/j',
+    require => User['alexis'],
   }
 }
