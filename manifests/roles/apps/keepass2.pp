acb249a45c6d27de24a08423838a3770f517032b
Britt Gresham <britt@brittg.com>
2020-10-22 10:23:12 -0700

    Install keepassxc via snap

--- manifests/roles/apps/keepass2.pp
+++ manifests/roles/apps/keepass2.pp
@@ -1,26 +1,37 @@
 class profiles::roles::apps::keepass2 () {
+
   case $::osfamily {
-    'Darwin': { $keepass2 = 'keepassxc' }
+    'Darwin': {
+      $keepass2 = 'keepassxc'
+      $opts = {}
+    }
+    'Debian': {
+      case $facts['os']['release']['major'] {
+        '20.04': {
+          $additional_packages = ['xdotool']
+          $opts = { provider => 'snap' }
+        }
+        '18.04': {
+          $additional_packages = ['xdotool']
+          $opts = { provider => 'snap' }
+        }
+        default: {
+          $additional_packages = []
+          $opts = {}
+        }
+      }
+    }
     default: {
       $keepass2 = 'keepassxc'
       package { 'keepass2':
         ensure => absent,
       }
-    }
-  }
-
-  case $::osfamily {
-    'Debian': {
-      case $facts['os']['release']['major'] {
-        '18.04': { $additional_packages = ['xdotool'] }
-        default: { $additional_packages = [] }
-      }
-    }
-    default: {
       $additional_packages = []
     }
   }
 
-  package { $keepass2: }
+  package { $keepass2:
+    * => $opts,
+  }
   package { $additional_packages: }
 }
